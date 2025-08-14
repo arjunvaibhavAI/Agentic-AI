85ded406ed95a7280b47166fc77517b004f1120f
arjunvaibhavAI <arjunvaibhav.ai@gmail.com>
2025-08-14 23:28:02 +0530

    Delete Agentic-AI directory

--- Agentic-AI/FinSight-Agent/llm.py
+++
@@ -1,9 +0,0 @@
-from langchain_groq import ChatGroq
-from config import LLM_MODEL_NAME, GROQ_API_KEY
-
-
-def init_llm():
-    return ChatGroq(
-        model=LLM_MODEL_NAME,
-        api_key=GROQ_API_KEY
-    )
